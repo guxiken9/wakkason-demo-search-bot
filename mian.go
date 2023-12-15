5f98f74bc8160dbe4cb4b6ac17468635aa759808
guxiken9 <guxiken9@gmail.com>
2023-12-15 11:35:36 +0900

    検索機能の外側実装

--- mian.go
+++ mian.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
-func HandleRequest(event LambdaFunctionURLRequest) (string, error) {
-	fmt.Println("### Start ###")
+func replyToLINE(l LineRequestBody, m string) error {
 
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_ACCESS_TOKEN"),
+	)
+	if err != nil {
+		return err
+	}
+
+	token := l.Events[0].ReplyToken
+	if _, err := bot.ReplyMessage(token, linebot.NewTextMessage(m)).Do(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func HandleRequest(event LambdaFunctionURLRequest) (string, error) {
+	slog.Info("Start")
 	// JSON のデコード
-	_, err := UnmarshalLineRequestBody([]byte(event.Body))
+	l, err := UnmarshalLineRequestBody([]byte(event.Body))
 	if err != nil {
 		return "", err
 	}
@@ -20,11 +38,17 @@ func HandleRequest(event LambdaFunctionURLRequest) (string, error) {
 	// 検索キーワードを元に検索を実行
 
 	// 検索結果をLINEあてに返却
+	if err := replyToLINE(l, "TEST"); err != nil {
+		slog.Error("Reply to LINE Error", err)
+		return "", err
+	}
 
-	fmt.Println("### End ###")
+	slog.Info("End")
 	return "### success ###", nil
 }
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
 	lambda.Start(HandleRequest)
 }
