ccfea102df8ec48b3abaf42bac74c3d9b54a7801
annkara <howtobecomeahacker@gmail.com>
2023-12-17 10:26:29 +0900

    Kendraの検索処理を実装

--- go.mod
+++ go.mod
@@ -5,3 +5,8 @@ go 1.21.4
 require github.com/aws/aws-lambda-go v1.42.0
 
 require github.com/line/line-bot-sdk-go/v7 v7.21.0
+
+require (
+	github.com/aws/aws-sdk-go v1.49.4 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- mian.go
+++ mian.go
@@ -5,8 +5,48 @@ import (
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kendra"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
+
+const AWS_REGION = "ap-northeast-1"
+const KENDRA_INDEX_ID = "eb17ff24-49b1-4851-8285-9b16b2ffa6a4"
+
+func newKendraClient() (*kendra.Kendra, error) {
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(AWS_REGION),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	client := kendra.New(sess)
+	return client, nil
+
+}
+
+func kendraSearch(keyword string) (*kendra.QueryOutput, error) {
+
+	c, err := newKendraClient()
+	if err != nil {
+		return nil, err
+	}
+
+	input := &kendra.QueryInput{
+		QueryText: aws.String(keyword),
+		IndexId:   aws.String(KENDRA_INDEX_ID),
+	}
+
+	result, err := c.Query(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
 
 func replyToLINE(l LineRequestBody, m string) error {
 
@@ -34,11 +74,16 @@ func HandleRequest(event LambdaFunctionURLRequest) (string, error) {
 	}
 	searchWord := l.Events[0].Message.Text
 
-	// 検索キーワードを元に検索を実行
-
+	// Kendra で検索
+	result, err := kendraSearch(searchWord)
+	if err != nil {
+		slog.Error("Kendra Search Error ", err)
+		return "", err
+	}
+	slog.Info(result.String())
 	// 検索結果をLINEあてに返却
 	if err := replyToLINE(l, searchWord); err != nil {
-		slog.Error("Reply to LINE Error", err)
+		slog.Error("Reply to LINE Error ", err)
 		return "", err
 	}
 
